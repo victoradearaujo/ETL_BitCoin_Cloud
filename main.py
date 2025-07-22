dbe9521d03858b2d5fefd933f748b767d19263ff
Victor Araujo <victoraugusto.a@gmail.com>
2025-07-22 20:29:00 +1000

    commit requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from etl_bitcoin import extract_data_bitcoin, transform_data_bitcoin
 from database import engine, sessionmaker, BitcoinPrice
 from datetime import datetime
+from time import sleep
 
-def main():
+def save_bitcoin_data():
     raw_data = extract_data_bitcoin()
     transformed = transform_data_bitcoin(raw_data)
     
@@ -16,7 +17,19 @@ def main():
     
     session.add(bitcoin)
     session.commit()
-    print('Data Saved')
+    session.close
+    print(f'Data Saved on {datetime.now().replace(microsecond=0)}')
+
+
+def main():
+    while True:
+        try:
+            save_bitcoin_data()
+        except Exception as error_data:
+            print(f'Error Data Save: {error_data}')
+                  
+        sleep(3600)
+        
 
 if __name__ == '__main__':
     main()
